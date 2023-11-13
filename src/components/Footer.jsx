1b80dee69f6d9cf382b0db719722b18b350cefcb
leonardo Santos <leofrrodrigues86@gmail.com>
2023-11-13 10:11:04 -0300

    Correção de bugs na página inicial solucionado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full h-28 flex justify-center items-center  relative top-24 left-0 text-[black]">
+    <footer className="w-full h-28 flex justify-center items-center  relative top-24 left-0 text-[black] max-md:text-center">
       <h4>©2023 por Leonardo Santos. Desenvolvedor Front-end</h4>
     </footer>
   );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,17 +1,23 @@
 import Footer from "../components/Footer";
 
-import { BiLogoLinkedin, BiLogoInstagram, BiLogoTwitter } from "react-icons/bi";
-
 import image1 from "../assets/1.jpeg";
-import image4 from "../assets/portifolio.png";
+import image4 from "../assets/4.jpeg";
+
+import {
+  BiLogoLinkedin,
+  BiLogoInstagram,
+  BiLogoTwitter,
+  BiWorld,
+  BiPlayCircle,
+} from "react-icons/bi";
 
 const Home = () => {
   return (
     <>
-      <main className="w-full mt-12  text-[#94857B]">
-        <section className="w-full h-[980px]  flex items-center justify-center  ">
-          <div className="w-full h-[820px] flex justify-center">
-            <div className="w-[490px] h-[820px] p-8 bg-[#1F2122] rounded-3xl max-lg:hidden ">
+      <main className="w-full mt-4 text-[#94857B]">
+        <section className="w-full h-full flex items-center justify-center ">
+          <div className="w-full h-full flex flex-wrap justify-center  items-center py-5 mt-24">
+            <div className="w-[490px] h-[820px] p-8 bg-[#1F2122] rounded-3xl max-lg:h-full max-lg:w-[90%]">
               <img
                 src={image1}
                 alt="Minha imagem pessoal"
@@ -19,8 +25,10 @@ const Home = () => {
               />
             </div>
             <div className="w-[490px] h-[820px] text-center p-8 bg-[#1F2122] rounded-3xl flex flex-col justify-center max-lg:w-[90%] items-center ">
-              <h2 className="mb-2 text-8xl text-[#8F5D46]">Daniela</h2>
-              <span className="mb-10">Jornalista|Redatora|Repórter</span>
+              <h2 className="mb-2 text-8xl text-[#8F5D46] max-md:text-7xl">
+                Daniela
+              </h2>
+              <span className="mb-10">Jornalista | Redatora | Repórter</span>
               <p className="text-justify">
                 Olá! Sou Daniela, uma jornalista em formação apaixonada pela
                 arte da comunicação. Minha jornada profissional é uma mistura de
@@ -35,48 +43,59 @@ const Home = () => {
             </div>
           </div>
         </section>
-        <section className="w-full h-[781px] flex bg-[#1F2122] max-lg:flex-col max-lg:h-full justify-center items-center max-lg:w-[90%] rounded-3xl">
-          <div className="w-2/4 px-8  ">
-            <img
-              src={image4}
-              alt="alguma imagem"
-              className="w-full h-[781px] max-lg:h-full"
-            />
-          </div>
-          <div className=" w-2/4 px-8 h-full text-center p-8 bg-[#1F2122] rounded-3xl flex flex-col justify-center ">
-            <h2 className="mb-2 text-4xl text-[#8F5D46]">Portifólio</h2>
-            <p className="text-justify mb-5">
-              Aqui, neste espaço, convido você a explorar alguns desses
-              conteúdos. Cada projeto é um reflexo do meu compromisso com a
-              autenticidade, profundidade e inovação. Cada palavra escrita e
-              cada imagem capturada são testemunhos do meu desejo de
-              compartilhar o extraordinário e encontrar significado no
-              aparentemente comum. Seja bem-vindo e descubra o mundo através dos
-              meus olhos e palavras
-            </p>
-            <a
-              href="https://cursoreportertv.com.br/novo/"
-              target="_blank"
-              rel="noreferrer"
-              className="border mb-3"
-            >
-              <p>icone-Reporter-Tv</p>
-            </a>
-            <a
-              href="https://www.rbtv.com.br/"
-              target="_blank"
-              rel="noreferrer"
-              className=" border"
-            >
-              <p>icone-RBTV</p>
-            </a>
+
+        <section className="w-full h-full flex items-center justify-center ">
+          <div className="w-full h-full flex flex-wrap justify-center  items-center py-5 mt-24">
+            <div className="w-[490px] h-[820px] p-8 bg-[#1F2122] rounded-3xl max-lg:h-full max-lg:w-[90%]">
+              <img
+                src={image4}
+                alt="Minha imagem pessoal"
+                className="w-full h-full "
+              />
+            </div>
+            <div className="w-[490px] h-[820px] text-center p-8 bg-[#1F2122] rounded-3xl flex flex-col justify-center max-lg:w-[90%] items-center ">
+              <h2 className="mb-2 text-8xl text-[#8F5D46] max-md:text-6xl">
+                Portifólio
+              </h2>
+              <p className="text-justify py-5">
+                Aqui, neste espaço, convido você a explorar alguns desses
+                conteúdos. Cada projeto é um reflexo do meu compromisso com a
+                autenticidade, profundidade e inovação. Cada palavra escrita e
+                cada imagem capturada são testemunhos do meu desejo de
+                compartilhar o extraordinário e encontrar significado no
+                aparentemente comum. Seja bem-vindo e descubra o mundo através
+                dos meus olhos e palavras.
+              </p>
+              <div className="flex flex-wrap justify-center items-center gap-4">
+                <a
+                  href="https://cursoreportertv.com.br/novo/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex flx-wrap gap-3 items-center justify-center border rounded-md p-2"
+                >
+                  <p>Curso Reporter de TV</p>
+                  <BiWorld size={25} />
+                </a>
+                <a
+                  href="https://www.rbtv.com.br/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex flx-wrap gap-3 items-center justify-center border rounded-md p-2"
+                >
+                  <p>Rede Brasil</p>
+                  <BiPlayCircle size={25} />
+                </a>
+              </div>
+            </div>
           </div>
         </section>
+
         <section className="w-full h-[460px] flex flex-col items-center justify-center ">
           <div className="w-[980px] h-[340px] flex flex-col items-center justify-center max-lg:w-[90%] rounded-3xl gap-3 bg-[#1F2122]">
             <h2 className="uppercase text-4xl">CONTATO</h2>
             <p className="text-sm">E-mail: ⤵️</p>
-            <p className="text-sm"> danielagentil@icloud.com</p>
+            <p className="text-sm">danielagentil@icloud.com</p>
+
             <div className="flex gap-3">
               <a
                 href="https://www.linkedin.com/in/danielagentil/"
@@ -102,6 +121,7 @@ const Home = () => {
             </div>
           </div>
         </section>
+
         <Footer />
       </main>
     </>
